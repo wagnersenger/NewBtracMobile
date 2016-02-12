88615f3e0d1241f74e6fb1036cf93992364735da
wagnersenger <wagnersenger@gmail.com>
2016-02-12 11:35:08 -0200

    12747 - Aplicativo Btrac

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 var _HOST_URL = 'http://mobile.btrac.com.br/vc/';
-//var _HOST_URL = 'http://192.168.146.131:8083/vc/';
-var _CURRENT_VERSION = '1.0.4';
+//var _HOST_URL = 'http://192.168.146.134:8083/vc/';
+//var _HOST_URL = 'http://192.168.123.107:8083/vc/';
+var _CURRENT_VERSION = '1.0.15';
 var _GMAPS_KEY = 'AIzaSyABBAEz0olIUCgvbZlCSuAPwkq8brNQXd8';
 var _GMAPS_ADDRESS_KEY = 'AIzaSyDf9xnK5US5l1xwrD66IJPauQrsAYrv25k';
 
