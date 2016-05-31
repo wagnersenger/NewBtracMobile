22da229c60edb0c9d68221ffac8f207221ba0ae5
Wagner Senger <wagnersenger@gmail.com>
2016-05-31 17:00:17 -0300

    Adicionado envio de comandos

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
-var _HOST_URL = 'http://mobile.btrac.com.br/1.0.18/';
+var _HOST_URL = 'http://mobile.btrac.com.br/1.0.19/';
 //var _HOST_URL = 'http://192.168.146.134:8083/1.0.18/';
 //var _HOST_URL = 'http://192.168.123.107:8083/1.0.18/';
-var _CURRENT_VERSION = '1.0.18';
+var _CURRENT_VERSION = '1.0.19';
 var _GMAPS_KEY = 'AIzaSyABBAEz0olIUCgvbZlCSuAPwkq8brNQXd8';
 var _GMAPS_ADDRESS_KEY = 'AIzaSyDf9xnK5US5l1xwrD66IJPauQrsAYrv25k';
 
