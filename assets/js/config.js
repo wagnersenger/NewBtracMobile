07fd3e829c8f645291a6aedc55a8f5803198b4db
Wagner Senger <wagnersenger@gmail.com>
2016-08-22 16:52:57 -0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,7 +1,7 @@
-var _HOST_URL = 'http://mobile.btrac.com.br/1.1.2/';
+var _HOST_URL = 'http://mobile.btrac.com.br/1.1.3/';
 //var _HOST_URL = 'http://192.168.146.134:8083/1.0.18/';
 //var _HOST_URL = 'http://192.168.32.131:8083/1.1.2/';
-var _CURRENT_VERSION = '1.1.2';
+var _CURRENT_VERSION = '1.1.3';
 var _GMAPS_KEY = 'AIzaSyABBAEz0olIUCgvbZlCSuAPwkq8brNQXd8';
 var _GMAPS_ADDRESS_KEY = 'AIzaSyDf9xnK5US5l1xwrD66IJPauQrsAYrv25k';
 
